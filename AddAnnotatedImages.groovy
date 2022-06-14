3c403c11cc3f0d9b3d9488c9af95e8cb6c75f5ea
SimoCanc <simocance@gmail.com>
2022-06-14 10:37:02 +0200

    Add annotated images in a project
    Add annotated images in a specified folder to a QuPath project

---
+++ AddAnnotatedImages.groovy
@@ -0,0 +1,14 @@
+def path = '\\path\\to\\folder\\with\\qpdata\\files'
+
+def project = getProject()
+def files = new File(path).listFiles().findAll {f -> f.isFile() && f.getName().endsWith('.qpdata')}
+for (file in files) {
+    def imageData = loadImageData(file.getAbsolutePath(), false)
+    def server = imageData.getServer()
+    def entry = project.addImage(server.getBuilder())
+    entry.setImageName(server.getMetadata().getName())
+    entry.setThumbnail(qupath.lib.gui.commands.ProjectCommands.getThumbnailRGB(server))
+    entry.saveImageData(imageData)
+}
+project.syncChanges()
+getQuPath().refreshProject()
