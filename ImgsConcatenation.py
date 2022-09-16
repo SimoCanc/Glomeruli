a5464380cc5dd9188164d0a3aa9b8c5cb2187ff6
SimoCanc <simocance@gmail.com>
2022-09-16 19:02:09 +0200

    Aggiunta scelta tipo di formato immagini
    Aggiunta scelta tipo di formato immagini: "jpg "o "png"

--- ImgsConcatenation.py
+++ ImgsConcatenation.py
@@ -8,12 +8,16 @@ import os
 os.mkdir('PROVA')
 dst = './PROVA'
 path = '../input/glomertopi/trainTiles/6533668'
-dim_test = 16
+
+formati_ammessi = ['jpg', 'png'] # Possibili formati delle immagini: jpg, png, ...
+FORMAT = 'jpg' 
+if FORMAT not in formati_ammessi: raise Exception('ATTENZIONE: Formato immagini sconosciuto!!!')
+imgs_format = '.' + FORMAT
 
 i = 0
 for _, _, filenames in os.walk(path):
     for filename in filenames:
-        if filename.endswith('.jpg') and i < dim_test:
+        if filename.endswith(imgs_format) and i < dim_test:
             shutil.copyfile(os.path.join(path, filename), os.path.join(dst, filename))
             i += 1
 
@@ -63,7 +67,7 @@ def sort_imgs_list(paths_to_imgs):
     foo_imgs_paths = sorted(foo_imgs_paths, key=lambda item: [int(part) for part in item.split('x')])
     foo_imgs_paths = ['../input/glomertopi/trainTiles/6533668/56_tile' +
                       elem for elem in foo_imgs_paths]
-    foo_imgs_paths = [elem + '.jpg' for elem in foo_imgs_paths]
+    foo_imgs_paths = [elem + imgs_format for elem in foo_imgs_paths]
     sorted_imgs_paths = foo_imgs_paths
     return sorted_imgs_paths
 
@@ -86,7 +90,7 @@ def concat_images(input_folder, output_folder, imgs_resize=None):
     """
     
     imgs_paths = [os.path.join(input_folder, f) 
-                   for f in os.listdir(input_folder) if f.endswith('.jpg')]
+                   for f in os.listdir(input_folder) if f.endswith(imgs_format)]
     imgs_paths = sort_imgs_list(imgs_paths)
     
     if imgs_resize:
@@ -118,10 +122,91 @@ def concat_images(input_folder, output_folder, imgs_resize=None):
             image.paste(images[idx], offset)
             
     # Salvataggio griglia
-    image.save(os.path.join(output_folder, 'canvas.jpg'), 'JPEG')
+    import os
+from PIL import Image, ImageOps
+import math
+
+def check_is_quadratic(input_list):
+    # Controlla che il numero di immagini nella cartella di input
+    # siano un quadrato perfetto
+    
+        if not math.sqrt(len(input_list)).is_integer(): 
+            print('ATTENZIONE: Numero di righe della griglia diverso dal numero di colonne')
+
+
+def check_dims(folder):
+    # Controlla che le immagini nella cartella di input
+    # abbiano tutte la stessa dimensione
+    
+    for _, _, filenames in os.walk(folder):
+        for i in range(len(filenames)-1):
+            if Image.open(os.path.join(path, filenames[i])).size != \
+               Image.open(os.path.join(path, filenames[i+1])).size:
+                print(f'ATTENZIONE: l\'immagine {filenames[i+1]} ha dimensioni diverse rispetto alle altre')
+                print('Controllare che tutte le immagini abbiano la stessa dimensione')
+
+
+def concat_images(input_folder, output_folder, imgs_resize=None):
+    """
+    Crea una griglia di immagini
+    
+    La griglia viene popolata da sinistra verso destra, e dall'alto vero il basso
+    con le immagini presenti nella cartella di input
+    
+    Parametri
+    ---------
+    input_folder : str
+        La cartella contenente le immagini con cui formare la griglia
+    output_folder : str
+        La cartella dove viene salvata l'immagine completa della griglia
+    imgs_resize : int, optional
+        Dimensione a cui ridimensionare le immagini che comporranno la griglia (di default a None)
+    
+    """
+    
+    imgs_paths = [os.path.join(input_folder, f) 
+                   for f in os.listdir(input_folder) if f.endswith(imgs_format)]
+    
+    if imgs_resize:
+        imgs_shapes =  Image.open(imgs_paths[0]).resize((imgs_resize, imgs_resize)).size
+    else:
+        imgs_shapes = Image.open(imgs_paths[0]).size
+        
+    canvas_shape = math.sqrt(len(imgs_paths))
+    canvas_shape = (canvas_shape, canvas_shape)
+    
+    # Sanity check delle immgini
+    check_is_quadratic(imgs_paths)    
+    check_dims(input_folder)
+    
+    # Creazione griglia
+    width, height = imgs_shapes
+    images = map(Image.open, imgs_paths)
+    images = [ImageOps.fit(image, imgs_shapes, Image.LANCZOS) 
+              for image in images]
+    
+    image_size = (width * canvas_shape[1], height * canvas_shape[0])
+    foo_dim = int(image_size[0])
+    image = Image.new('RGB', (foo_dim, foo_dim))
+    
+    for row in range(int(canvas_shape[0])):
+        for col in range(int(canvas_shape[1])):
+            offset = width * col, height * row
+            idx = int(row * canvas_shape[1] + col)
+            image.paste(images[idx], offset)
+            
+    # Salvataggio griglia
+    if FORMAT == 'jpg':
+        image.save(os.path.join(output_folder, 'canvas' + imgs_format), 'JPEG')
+    image.save(os.path.join(output_folder, 'canvas' + imgs_format), 'PNG')    
+
+concat_images('./CustomInput', './')
+
+# Visualizza la griglia di immagini
+#Image.open('canvas' + imgs_format)
 
 concat_images('./PROVA', './')
-Image.open('./canvas.jpg')
+Image.open('./canvas' + imgs_format)
 
 
 ###############################################################################################
